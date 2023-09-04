b4ce5e5426d6001e6234a7555cf6fce9dd52533f
gnanarabin <jnrabin001@gmail.com>
2023-09-04 20:23:08 +0530

    new

---
+++ Karka_JavaScript/8(30-08-23) Form/index.js
@@ -0,0 +1,17 @@
+const formdata={};
+    
+function get(){
+        const inputfields = document.querySelectorAll("input");
+        console.log('hai',inputfields);
+        const inputfield = document.querySelectorAll("textarea");
+        inputfields.forEach(input => {
+            formdata[input.name] =input.value;
+        })
+        inputfield.forEach(textarea => {
+            formdata[textarea.name] = textarea.value;
+        })
+        console.log(formdata)
+        str=JSON.stringify(formdata)
+        localStorage.setItem("data",str);
+
+    }
